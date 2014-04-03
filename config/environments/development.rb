c536c9ff2a54702784ea4773c525885faf634376
gregory <gregory@gregory-VirtualBox.(none)>
2014-04-04 04:10:47 +0530

    Added Redis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Freeseoreport::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #Redis ENV
+  ENV["REDISTOGO_URL"] = 'redis://redistogo:ff432456db28cc53b5b1da2a7abacadc@grideye.redistogo.com:9886'
 end
