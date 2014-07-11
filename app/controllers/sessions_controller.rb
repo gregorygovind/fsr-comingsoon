201ff3610d7f20dce034004aa9f449aa49ce5fa1
Philip De Smedt <philip.desmedt@gmail.com>
2014-07-11 14:01:34 +0200

    Login view.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,26 @@
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user, only: [:create]
+  skip_before_action :authenticate_user, only: [:new, :create]
 
   def new
   end
 
   def create
+    url = session[:return_to] || root_path
+
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      session[:return_to] = nil
+      respond_to do |format|
+        format.html { redirect_to url }
+        format.js { render js: "window.location.href = '#{url}'" }
+      end
+    else
+      respond_to do |format|
+        format.html { render :new }
+        format.js { render :new }
+      end
+    end
   end
 
   def destroy
