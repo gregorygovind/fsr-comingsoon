34d6eeb411a772d9f4ec9b381476402b9b8740af
Philip De Smedt <philip.desmedt@gmail.com>
2014-07-11 10:57:23 +0200

    Initial commit. Clean app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freeseoreport::Application.config.secret_key_base = '96372acc8e2165fbf1d911353abf7c6f9ed8885c74717513ebe7f514ac478834a76052b465070841366664e9a3223f60cd9146b043657d775494b2bc0c72b9f0'
