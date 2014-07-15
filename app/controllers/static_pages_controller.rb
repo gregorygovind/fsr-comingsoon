49076d8b6ddd9e21a50a9e29d3a8ac5c95079977
Roberto Simões <me@robertosimoes.net>
2014-07-15 13:29:40 -0300

    Coming soon page, changed routes...

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user
+  
+  def coming
+  end
 
   def landing
+  end
+  
+  def style 
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Freeseoreport::Application.routes.draw do
   constraints(AuthConstraint.new) do
     root :to => 'users#home', as: :authenticated_root
   end
-  root 'static_pages#landing'
+  root 'static_pages#coming'
+  
+  get '/landing', to: 'static_pages#landing'
+  get '/style', to: 'static_pages#style'
 
   resources :users, only: [:new, :create, :edit, :update] do
   end
